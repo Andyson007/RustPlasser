cd1ce70f4865ebb9d36116954df0f0dbba76fef6
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2023-12-31 15:18:01 +0100

    Working

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::{
     io::{self, BufWriter, Write},
     net::TcpListener,
     sync::Arc,
+    thread,
+    time::Duration,
 };
 
 use tokio::sync::{broadcast, Mutex};
@@ -44,15 +46,15 @@ async fn main() {
                     }
                 })
                 .collect::<Vec<usize>>()
-            })
+            }).rev()
             .collect::<Vec<Vec<usize>>>();
     for a in &history {
         println!("{a:?}")
     }
 
-    let (tx, _rx) = broadcast::channel::<Vec<usize>>(16);
+    let (tx, _rx) = broadcast::channel::<Vec<String>>(64);
     let transmitter = Arc::new(Mutex::new(tx.clone()));
-    let current = Arc::new(Mutex::new(history[0].clone()));
+    let current = Arc::new(Mutex::new(mapnames(&history[0], &names)));
     let current2 = Arc::clone(&current);
     let server = TcpListener::bind("0.0.0.0:9003").unwrap();
     tokio::spawn(async move {
@@ -64,15 +66,7 @@ async fn main() {
             let mut rx = tx.subscribe();
             let mut websocket = accept(stream.unwrap()).unwrap();
             let default = current.lock().await.clone();
-            websocket
-                .send(Message::from(
-                    default
-                        .iter()
-                        .map(|x| x.to_string())
-                        .collect::<Vec<String>>()
-                        .join(","),
-                ))
-                .unwrap();
+            websocket.send(Message::from(default.join(","))).unwrap();
             println!("{default:?}");
             tokio::spawn(async move {
                 println!("test");
@@ -88,7 +82,6 @@ async fn main() {
                         Ok(_) => (),
                         Err(_) => break,
                     }
-                    println!("{val:?}");
                 }
                 println!("Client has disconnected");
             })
@@ -100,24 +93,24 @@ async fn main() {
     loop {
         io::stdin().read_line(&mut String::new()).unwrap();
         let mut neighbours: HashMap<usize, HashSet<usize>> = HashMap::new();
-        neighbours.insert(history[0][0], HashSet::from([(history[0][1])]));
+        neighbours.insert(history[history.len()-1][0], HashSet::from([(history[history.len()-1][1])]));
         neighbours.insert(
-            history[0][history[0].len() - 1],
-            HashSet::from([(history[0][history[0].len() - 2])]),
+            history[history.len()-1][history[history.len()-1].len() - 1],
+            HashSet::from([(history[history.len()-1][history[history.len()-1].len() - 2])]),
         );
-        for seat in history[0].windows(3) {
+        for seat in history[history.len()-1].windows(3) {
             neighbours.insert(seat[1], HashSet::from([(seat[0]), (seat[2])]));
         }
-        let mut list = history[0].clone();
+        let mut list = history[history.len()-1].clone();
 
         let mut i = 0;
         loop {
             list.shuffle(&mut rand::thread_rng());
-            if !list.iter().zip(history[1].clone()).any(|(a, b)| *a == b)
+            if !list.iter().zip(&history[history.len()-2]).any(|(a, b)| *a == *b)
                 && !list
                     .iter()
-                    .zip(history[0].clone())
-                    .any(|(a, b)| section(*a) == section(b))
+                    .zip(&history[history.len()-1])
+                    .any(|(&a, &b)| section(a) == section(b))
                 && {
                     !list.windows(3).any(|arr| {
                         neighbours.get(&arr[1]).unwrap().contains(&arr[0])
@@ -134,20 +127,18 @@ async fn main() {
             i += 1;
         }
         println!("{i}");
-        println!("{list:?}");
+        history.push(list.clone());
         let list = list
             .iter()
             .take(8)
             .chain(list.iter().skip(8).rev())
             .map(|x| *x)
             .collect::<Vec<usize>>();
-        for &i in &list {
-            print!("{}, ", names[i].escape_default())
-        }
-        println!();
-        *current.lock().await = list.clone();
-        tx.send(list.clone()).unwrap();
-        history.insert(0, list);
+        println!("{list:?}");
+        let names = mapnames(&list, &names);
+        println!("{:?}", names);
+        *current.lock().await = names.clone();
+        tx.send(names).unwrap();
         // write_history(&json!({"history": history}));
     }
 }
@@ -166,4 +157,12 @@ fn write_history(to_write: &Value) {
     );
     serde_json::to_writer_pretty(&mut writer, &to_write).unwrap();
     writer.flush().unwrap();
+}
+
+fn mapnames(indicies: &Vec<usize>, names: &Vec<&str>) -> Vec<String> {
+    let mut ret = Vec::new();
+    for &i in indicies {
+        ret.push(names[i].to_string());
+    }
+    ret
 }
