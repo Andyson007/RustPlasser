161bd1e8156ecf13bdc9da3588e0c210deb6acb2
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-01-04 19:41:37 +0100

    Made cli work in addition to including itertools and splitting some things off into functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use serde_json::{json, Value};
 use std::{
     collections::{HashMap, HashSet},
@@ -96,70 +97,68 @@ async fn main() {
         }
     });
 
+    let mut list = history[history.len() - 1].clone();
+    let mut neighbours: HashMap<usize, HashSet<usize>> = generate_neighbours(&history);
     loop {
-        io::stdin().read_line(&mut String::new()).unwrap();
-        let mut neighbours: HashMap<usize, HashSet<usize>> = HashMap::new();
-        neighbours.insert(
-            history[history.len() - 2][0],
-            HashSet::from([(history[history.len() - 2][1])]),
-        );
-        neighbours.insert(
-            history[history.len() - 2][history[history.len() - 2].len() - 1],
-            HashSet::from([(history[history.len() - 2][history[history.len() - 2].len() - 2])]),
-        );
-        for seat in history[history.len() - 2].windows(3) {
-            neighbours.insert(seat[1], HashSet::from([(seat[0]), (seat[2])]));
-        }
-        for neigbour in &neighbours {
-            print!("({}: ", names[*neigbour.0]);
-            for v in neigbour.1 {
-                print!(" {}", names[*v]);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let split_input = input.split_whitespace().collect::<Vec<&str>>();
+        match split_input.iter().nth(0) {
+            Some(&"write") => {
+                if *history.last().expect("history has 0 length") == list {
+                    println!("Already pushed");
+                } else {
+                    println!("pushing");
+                    history.push(list.clone());
+                    neighbours = generate_neighbours(&history);
+                    // write_history(&json!({"history": history}));
+                }
             }
-            print!("), ");
-            println!();
-        }
-        let mut list = history[history.len() - 1].clone();
-
-        let mut i = 0;
-        loop {
-            list.shuffle(&mut rand::thread_rng());
-            if !list
-                .iter()
-                .zip(&history[history.len() - 2])
-                .any(|(a, b)| *a == *b)
-                && !list
+            _ => {
+                let iters = split_input
                     .iter()
-                    .zip(&history[history.len() - 1])
-                    .any(|(&a, &b)| {
-                        section(
-                            history[history.len() - 1]
+                    .nth(0)
+                    .unwrap_or(&"1")
+                    .parse::<u64>()
+                    .unwrap_or(1);
+                let sleep_time = split_input
+                    .iter()
+                    .nth(1)
+                    .unwrap_or(&"500")
+                    .parse::<u64>()
+                    .unwrap_or(500);
+                let previous_seating = &history[history.len() - 2];
+                let current_seating = &history[history.len() - 1];
+
+                for iter in 0..iters {
+                    if iter > 1 {
+                        thread::sleep(std::time::Duration::from_millis(sleep_time));
+                    }
+                    let mut i = 0;
+                    loop {
+                        list.shuffle(&mut rand::thread_rng());
+                        if !list.iter().zip(previous_seating).any(|(a, b)| *a == *b)
+                            && !list.iter().zip(current_seating).any(|(&a, &b)| {
+                                section(current_seating.iter().position(|x| *x == a).unwrap())
+                                    == section(current_seating.iter().position(|x| *x == b).unwrap())
+                            })
+                            && !list
                                 .iter()
-                                .position(|x| *x == a)
-                                .unwrap(),
-                        ) == section(
-                            history[history.len() - 1]
-                                .iter()
-                                .position(|x| *x == b)
-                                .unwrap(),
-                        )
-                    })
-                && !list
-                    .windows(2)
-                    .map(|arr| (arr[0], arr[1]))
-                    .any(|(a, b)| neighbours.get(&a).unwrap().contains(&b))
-            {
-                break;
+                                .tuple_windows()
+                                .any(|(a, b)| neighbours.get(&a).unwrap().contains(&b))
+                        {
+                            break;
+                        }
+                        i += 1;
+                    }
+                    println!("{i}");
+                    let names = mapnames(&fliplast(&list), &names);
+                    *current.lock().unwrap() = names.clone();
+                    tx.send(names).unwrap();
+                }
+                println!("done");
             }
-            i += 1;
         }
-        // write_history(&json!({"history": history}));
-        println!("{i}");
-        history.push(list.clone());
-        println!("{list:?}");
-        println!("{:?}", mapnames(&list, &names));
-        let names = mapnames(&fliplast(&list), &names);
-        *current.lock().unwrap() = names.clone();
-        tx.send(names).unwrap();
     }
 }
 
@@ -193,4 +192,28 @@ fn mapnames(indicies: &Vec<usize>, names: &Vec<&str>) -> Vec<String> {
         ret.push(names[i].to_string());
     }
     ret
+}
+
+fn generate_neighbours(history: &Vec<Vec<usize>>) -> HashMap<usize, HashSet<usize>> {
+    let mut neighbours: HashMap<usize, HashSet<usize>> = HashMap::new();
+    neighbours.insert(
+        history[history.len() - 2][0],
+        HashSet::from([(history[history.len() - 2][1])]),
+    );
+    neighbours.insert(
+        history[history.len() - 2][history[history.len() - 2].len() - 1],
+        HashSet::from([(history[history.len() - 2][history[history.len() - 2].len() - 2])]),
+    );
+    for seat in history[history.len() - 2].windows(3) {
+        neighbours.insert(seat[1], HashSet::from([(seat[0]), (seat[2])]));
+    }
+    // for neigbour in &neighbours {
+    //     print!("({}: ", *neigbour.0);
+    //     for v in neigbour.1 {
+    //         print!(" {}", *v);
+    //     }
+    //     print!("), ");
+    //     println!();
+    // }
+    neighbours
 }
