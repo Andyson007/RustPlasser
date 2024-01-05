e063d9e7bdb3ef5cace223e399b65e63eac5cbbb
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-01-05 09:38:06 +0100

    Works

--- src/main.rs
+++ src/main.rs
@@ -46,75 +46,57 @@ async fn main() {
                     }
                 })
                 .collect::<Vec<usize>>()
-            }).rev()
+            })
             .collect::<Vec<Vec<usize>>>();
+
     for a in &history {
         println!("{a:?}")
     }
 
     let (tx, _rx) = broadcast::channel::<Vec<String>>(1024);
     let transmitter = Arc::new(Mutex::new(tx.clone()));
-    let current = Arc::new(Mutex::new(mapnames(&fliplast(&history[0]), &names)));
+    let current = Arc::new(Mutex::new(mapnames(
+        &fliplast(&history.last().unwrap()),
+        &names,
+    )));
     let current2 = Arc::clone(&current);
     let server = TcpListener::bind("0.0.0.0:9003").unwrap();
-    tokio::spawn(async move {
-        for stream in server.incoming() {
-            println!("Client connected!");
-            let transmitter = Arc::clone(&transmitter);
-            let current = Arc::clone(&current2);
-            let tx = transmitter.lock().unwrap();
-            let mut rx = tx.subscribe();
-            let stream = stream.unwrap();
-            let ip = stream.peer_addr().unwrap();
-            let mut websocket = match accept(stream) {
-                Ok(x) => x,
-                Err(x) => {
-                    println!("Handshakerror {x:?}");
-                    continue;
-                }
-            };
-            let default = current.lock().unwrap().clone();
-            println!("Sendt {} to {ip:?}", default.join(","));
-            websocket.send(Message::from(default.join(","))).unwrap();
-            {
-                thread::spawn(move || {
-                    loop {
-                        let val = rx.blocking_recv().unwrap();
-                        let tosend = val
-                            .iter()
-                            .map(|x| x.to_string())
-                            .collect::<Vec<String>>()
-                            .join(",");
-                        println!("Sendt {tosend} to {ip:?}");
-                        match websocket.send(Message::from(tosend)) {
-                            Ok(_) => (),
-                            Err(_) => break,
-                        }
-                    }
-                    println!("Client has disconnected");
-                });
-            }
-        }
-    });
+    tokio::spawn(async { serve_server(server, current2, transmitter) });
 
     let mut list = history[history.len() - 1].clone();
-    let mut neighbours: HashMap<usize, HashSet<usize>> = generate_neighbours(&history);
+    let mut neighbours: HashMap<usize, HashSet<usize>> =
+        generate_neighbours(&history[history.len() - 1]);
     loop {
+        let previous_seating = &history[history.len() - 2];
+        let current_seating = &history[history.len() - 1];
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        let split_input = input.split_whitespace().collect::<Vec<&str>>();
-        match split_input.iter().nth(0) {
-            Some(&"write") => {
-                if *history.last().expect("history has 0 length") == list {
+        let mut split_input = input.split_whitespace().collect::<Vec<&str>>();
+        match *split_input.iter().nth(0).unwrap_or(&"Regular") {
+            "write" => {
+                if *current_seating == list {
                     println!("Already pushed");
                 } else {
                     println!("pushing");
                     history.push(list.clone());
-                    neighbours = generate_neighbours(&history);
-                    // write_history(&json!({"history": history}));
+                    neighbours = generate_neighbours(&list);
+                    println!("Do you want to write to the json file? (yes/no) full words");
+                    let mut ans = String::new();
+                    io::stdin()
+                        .read_line(&mut ans)
+                        .expect("Failed to read from stdin");
+                    if ans.lines().nth(0).unwrap() == "yes" {
+                        write_history(&json!({"history": history}));
+                        println!("Wrote to json");
+                    }
+                    println!("{history:?}")
                 }
             }
             _ => {
+                if split_input.iter().nth(0) == Some(&"Regular") {
+                    split_input.remove(0);
+                }
                 let iters = split_input
                     .iter()
                     .nth(0)
@@ -127,31 +109,16 @@ async fn main() {
                     .unwrap_or(&"500")
                     .parse::<u64>()
                     .unwrap_or(500);
-                let previous_seating = &history[history.len() - 2];
-                let current_seating = &history[history.len() - 1];
 
                 for iter in 0..iters {
                     if iter > 1 {
                         thread::sleep(std::time::Duration::from_millis(sleep_time));
                     }
-                    let mut i = 0;
-                    loop {
-                        list.shuffle(&mut rand::thread_rng());
-                        if !list.iter().zip(previous_seating).any(|(a, b)| *a == *b)
-                            && !list.iter().zip(current_seating).any(|(&a, &b)| {
-                                section(current_seating.iter().position(|x| *x == a).unwrap())
-                                    == section(current_seating.iter().position(|x| *x == b).unwrap())
-                            })
-                            && !list
-                                .iter()
-                                .tuple_windows()
-                                .any(|(a, b)| neighbours.get(&a).unwrap().contains(&b))
-                        {
-                            break;
-                        }
-                        i += 1;
-                    }
-                    println!("{i}");
+                    generate_seating(
+                        &mut list,
+                        &[&previous_seating, &current_seating],
+                        &neighbours,
+                    );
                     let names = mapnames(&fliplast(&list), &names);
                     *current.lock().unwrap() = names.clone();
                     tx.send(names).unwrap();
@@ -194,17 +161,14 @@ fn mapnames(indicies: &Vec<usize>, names: &Vec<&str>) -> Vec<String> {
     ret
 }
 
-fn generate_neighbours(history: &Vec<Vec<usize>>) -> HashMap<usize, HashSet<usize>> {
+fn generate_neighbours(seating: &Vec<usize>) -> HashMap<usize, HashSet<usize>> {
     let mut neighbours: HashMap<usize, HashSet<usize>> = HashMap::new();
+    neighbours.insert(seating[0], HashSet::from([seating[1]]));
     neighbours.insert(
-        history[history.len() - 2][0],
-        HashSet::from([(history[history.len() - 2][1])]),
+        seating[seating.len() - 1],
+        HashSet::from([(seating[seating.len() - 2])]),
     );
-    neighbours.insert(
-        history[history.len() - 2][history[history.len() - 2].len() - 1],
-        HashSet::from([(history[history.len() - 2][history[history.len() - 2].len() - 2])]),
-    );
-    for seat in history[history.len() - 2].windows(3) {
+    for seat in seating.windows(3) {
         neighbours.insert(seat[1], HashSet::from([(seat[0]), (seat[2])]));
     }
     // for neigbour in &neighbours {
@@ -217,3 +181,87 @@ fn generate_neighbours(history: &Vec<Vec<usize>>) -> HashMap<usize, HashSet<usiz
     // }
     neighbours
 }
+
+fn serve_server(
+    server: TcpListener,
+    current: Arc<Mutex<Vec<String>>>,
+    transmitter: Arc<Mutex<broadcast::Sender<Vec<String>>>>,
+) {
+    for stream in server.incoming() {
+        println!("Client connected!");
+        let tx = transmitter.lock().unwrap();
+        let mut rx = tx.subscribe();
+        let stream = stream.unwrap();
+        let ip = stream.peer_addr().unwrap();
+        let mut websocket = match accept(stream) {
+            Ok(x) => x,
+            Err(x) => {
+                println!("Handshakerror {x:?}");
+                continue;
+            }
+        };
+        let default = current.lock().unwrap().clone();
+        println!("Sendt {} to {ip:?}", default.join(","));
+        websocket.send(Message::from(default.join(","))).unwrap();
+        {
+            thread::spawn(move || {
+                loop {
+                    let val = rx.blocking_recv().unwrap();
+                    let tosend = val
+                        .iter()
+                        .map(|x| x.to_string())
+                        .collect::<Vec<String>>()
+                        .join(",");
+                    // println!("Sendt {tosend} to {ip:?}");
+                    match websocket.send(Message::from(tosend)) {
+                        Ok(_) => (),
+                        Err(_) => break,
+                    }
+                }
+                println!("Client has disconnected");
+            });
+        }
+    }
+}
+
+fn generate_seating(
+    list: &mut Vec<usize>,
+    seating: &[&Vec<usize>; 2],
+    neighbours: &HashMap<usize, HashSet<usize>>,
+) {
+    let (previous_seating, current_seating) = (seating[0], seating[1]);
+    let mut i = 0;
+    loop {
+        i += 1;
+        list.shuffle(&mut rand::thread_rng());
+        if !list.iter().zip(previous_seating).any(|(a, b)| *a == *b)
+            && !list.iter().zip(current_seating).any(|(&a, &b)| {
+                section(current_seating.iter().position(|x| *x == a).unwrap())
+                    == section(current_seating.iter().position(|x| *x == b).unwrap())
+            })
+            && !list
+                .iter()
+                .tuple_windows()
+                .any(|(a, b)| neighbours.get(&a).unwrap().contains(&b))
+        {
+            break;
+        }
+    }
+    println!("{i}");
+}
+
+// enum Opt {
+//     Value(usize),
+//     Other(String),
+// }
+//
+// enum Input{
+//     Write(Vec<Opt>),
+//     Scramble(Vec<Opt>),
+// }
+//
+// impl Input {
+//   pub fn read() -> Self {
+//
+//   }
+// }
